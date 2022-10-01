516cbf6d7f7d1f902d89774585e1bef3179bb107
Jonwhite002 <jonathon.white@stonybrook.edu>
2022-09-30 23:47:59 -0400

    Finished HW2, and revised inputs using << operator

--- homeworks/homework_2/Input_Output.cpp
+++ homeworks/homework_2/Input_Output.cpp
@@ -1,37 +1,28 @@
 #include <iostream>
 #include <string>
-#include <vector>
-#include <sstream>
 
 int main() {
     
     // Inputs
-    std::string title, name, date, bio;
+    std::string title, firstName, lastName, month, day, year, bio;
 
-    // Ask user for their title
-    std::cout << "What is your title?" << std::endl;
-    std::getline(std::cin, title);
+    // Ask user for their title and name
+    std::cout << "What is your title, first name and last name?" << std::endl;
+    std::cin >> title >> firstName >> lastName;
 
     // Ask user for their date of birth
-    std::cout << "What is your date of birth? (MM\\DD\\YY)" << std::endl;
-    std::getline(std::cin, date);
-    // Split date into tokens w/ space character as delimiter & push into token vector
-    char delim = '\\';
-    std::istringstream split(date);
-    std::vector<std::string> date_Arr;
-    for(std::string parse; std::getline(split, parse, delim); date_Arr.push_back(parse));
-
-    std:: cout << "Tell me about yourself" << std::endl;
+    std::cout << "What is your date of birth? (month, day, year))" << std::endl;
+    std::cin >> month >> day >> year;
+    
+    // Ask user to fill in their bio
+    std::cin.ignore();
+    std:: cout << "Please fill in your bio:" << std::endl;
     std::getline(std::cin, bio);
 
-    // Print all the information
-    std::cout << title << std::endl;
-    std::cout << '\t' << "DOB: ";
-    for(int i = 0; i <= 2; i++)
-    {
-        std::cout << date_Arr[i] << '\\';
-    }
-    std::cout << '\n' << '\t' << "Bio: " << bio << std::endl;
+    // Print out all the information
+    std::cout << '\n' << title << ' ' << firstName << ' ' << lastName << std::endl;
+    std::cout << '\t' << "DOB: " << month << '\\' << day << '\\' << year << std::endl;
+    std::cout << '\t' << "Bio: " << bio << '\n' << std::endl;
 
     return 0;
 }
