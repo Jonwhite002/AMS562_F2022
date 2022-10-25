78530a22f83ba27c7ba8c009054c68b73dd3912a
Jon White <jonathon.white@stonybrook.edu>
2022-10-24 20:25:36 -0400

    Finished dotProduct & unitVec functions

--- homeworks/homework_3/DEBUG.cpp
+++ homeworks/homework_3/DEBUG.cpp
@@ -5,20 +5,22 @@ using namespace std;
 
 // prototype functions
 float dotProduct(float *A, float *B, int arr_sz);
-float norm(float *A, int arr_sz);
+void unitVec(float *A, float *vec, int arr_sz);
 
 int main(int argc, char *argv[]) {
     float A[3] = {2, 3, 4};
     float B[3] = {3, 7, 2};
+    float n1[3], n2[3];
     float l;
 
     int arr_sz = sizeof(A)/sizeof(float);
 
     // Length testing
-    
-    l = acos(dotProduct(A, B, arr_sz));
+    unitVec(A, n1, 3);
+    unitVec(B, n2, 3);
+    l = acos(dotProduct(n1, n2, arr_sz));
 
-    cout << dotProduct(A, B, arr_sz) << norm(A, arr_sz) << endl;
+    cout << dotProduct(n1, n2, arr_sz) << endl;
 
     return 0;
 }
@@ -35,13 +37,19 @@ float dotProduct(float *A, float *B, int arr_sz)
     return temp;
 }
 
-float norm(float *A, int arr_sz)
+void unitVec(float *A, float *vec, int arr_sz)
 {
-    float temp = 0.0;
+    // Find the norm
+    float norm, temp = 0.0;
     for (int i = 0; i < arr_sz; i++)
     {
         temp = temp + pow(A[i], 2);
     }
+    norm = sqrt(temp);
 
-    return sqrt(temp);
+    // Determine the unit vector
+    for (int i = 0; i < arr_sz; i++)
+    {
+        vec[i] = A[i]/norm;
+    }
 }
