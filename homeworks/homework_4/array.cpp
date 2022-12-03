8f0a93ee5235a4c025884721b26bcf8be42acdd4
Jon White <jonathon.white@stonybrook.edu>
2022-12-03 01:41:55 -0500

    Added additional functions

--- homeworks/homework_4/array.cpp
+++ homeworks/homework_4/array.cpp
@@ -36,6 +36,16 @@ double *Array::data() {return _data;}
 
 const double *Array::data() const {return _data;}
 
+void Array::copy(const Array &rhs) {
+    if (_size != rhs._size) {
+        std::cerr << "ERROR: Left and right hand sides are not of same size\n";
+        return;
+    }
+    for(int i = 0; i < _size; ++i) {
+        _data[i] = rhs._data[i];
+    }
+}
+
 double &Array::at(unsigned index) {
     if (index >= _size) {
         std::cerr << "Error: Index exceeds number of array elements. Index must not exceed "
@@ -54,24 +64,9 @@ const double &Array::at(unsigned index) const {
     return _data[index];
 }
 
-void Array::copy(const Array &rhs) {
-    if (_size != rhs._size) {
-        std::cerr << "ERROR: Left and right hand sides are not of same size\n";
-        return;
-    }
-    for(int i = 0; i < _size; ++i) {
-        _data[i] = rhs._data[i];
-    }
-}
+// void Array::resize(unsigned new_size, bool prsv = true) {
 
-
-void Array::print() const {
-    std::cout << "Size: " << _size << "\nElements: [ ";
-    for (int i = 0; i < _size; ++i) {
-        std::cout << _data[i] << " ";
-    }std::cout << "]" << std::endl;
-}
-
+// }
 
 double Array::norm() const {
     double temp = 0;
@@ -89,6 +84,22 @@ double Array::sum() const {
     return(temp);
 }
 
+double Array::max() const {
+    double temp = 0;
+    for (int i = 0; i < _size; ++i) {
+        temp = (temp < _data[i]) ? _data[i] : temp;
+    }
+    return temp;
+}
+
+double Array::min() const {
+    double temp = 0;
+    for (int i = 0; i < _size; ++i) {
+        temp = (temp > _data[i]) ? _data[i] : temp;
+    }
+    return temp;
+}
+
 double Array::dot(const Array &rhs) const {
     if (_size != rhs._size) {
         std::cerr << "ERROR: Left and right hand sides are not of same size\n";
@@ -102,7 +113,24 @@ double Array::dot(const Array &rhs) const {
 }
 
 Array Array::add(const Array &rhs) const {
-    for (int i = 0; i < _size; ++i) {
-        this->_data[i] = _data[i]+rhs._data[i];
+    Array arr(rhs._size);
+    for (int i = 0; i < arr._size; ++i) {
+        arr._data[i] = _data[i]+rhs._data[i];
     }
+    return(arr);
+}
+
+Array Array::sub(const Array &rhs) const {
+    Array arr(rhs._size);
+    for (int i = 0; i < arr._size; ++i) {
+        arr._data[i] = _data[i]-rhs._data[i];
+    }
+    return(arr);
+}
+
+void Array::print() const {
+    std::cout << "Size: " << _size << "\nElements: [ ";
+    for (int i = 0; i < _size; ++i) {
+        std::cout << _data[i] << " ";
+    }std::cout << "]" << std::endl;
 }
--- homeworks/homework_4/main.cpp
+++ homeworks/homework_4/main.cpp
@@ -6,9 +6,13 @@ int main() {
     Array arr1(3,3.2);
     Array arr2(4,2.7);
 
-    arr1.print();
+    arr1.at(0) = 2.4;
+    arr1.at(1) = -9.2;
+    arr1.at(2) = 5.9;
 
-    std::cout << arr1.at(4) << std::endl;
+    Array arr3 = arr1.add(arr2);
+
+    arr3.print();
 
     return 0;
 }
